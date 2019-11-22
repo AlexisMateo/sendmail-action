302a11bad2fdc870cf70b8363b7e611a4970d6f9
Alexis Mateo <alexismateod@outlook.com>
2019-11-22 10:39:09 -0400

    testing worflow

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const github = require('@actions/github');
 const nodemailer = require("nodemailer");
 
 try {
-    const smtpServer = core.getInput('smtpserver');
-    const smtpServerPort = core.getInput('smtpServerPort');
-    const authUser = core.getInput('authUser');
-    const authPassword = core.getInput('authPassword');
+    const smtpServer = core.getInput('smtp-server');
+    const smtpServerPort = core.getInput('smtp-server-port');
+    const authUser = core.getInput('auth-user');
+    const authPassword = core.getInput('auth-password');
     const subject = core.getInput('subject');
     const body = core.getInput('body');
     const from = core.getInput('from');
-    const isTLS = core.getInput('tls');
     const reciver = core.getInput('to');
+    const isTLS = core.getInput('tls');
 
     let transporter = nodemailer.createTransport({
         host: smtpServer,
