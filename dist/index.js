c2b41387dbc0015fad2ffd7308aaf866418bf3ad
Alexis Mateo <alexismateod@outlook.com>
2019-11-22 14:18:21 -0400

    testing worflow

--- dist/index.js
+++ dist/index.js
@@ -427,19 +427,12 @@ try {
     let from = core.getInput('from');
     let reciver = core.getInput('to');
     let isTLS = core.getInput('tls');
-    core.setOutput("smtpServer", smtpServer);
-    core.setOutput("smtpServerPort", smtpServerPort);
-    core.setOutput("authUser", authUser);
-    core.setOutput("authPassword", authPassword);
-    core.setOutput("subject", subject);
-    core.setOutput("body", body);
-    core.setOutput("from", from);
-    core.setOutput("reciver", reciver);
-    core.setOutput("isTLS", isTLS);
+
+
     let transporter = nodemailer.createTransport({
         host: smtpServer,
         port: smtpServerPort,
-        secure: isTLS, 
+        secure: false, 
         auth: {
             user: authUser,
             pass: authPassword
@@ -454,7 +447,11 @@ try {
     };
 
 
-    transporter.sendMail(message)
+    transporter.sendMail(message).then(function(res){
+        core.setOutput("response", message);
+    }).catch(function(error){
+        core.setOutput("error", message);
+    });
 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
